9d672f4df2556c4d778884174ce905d8c67d1697
Ahmed T. Ali <ah.tajelsir@gmail.com>
2018-01-16 13:33:27 +0200

    git-todos version v0.2.2

--- Formula/git-todos.rb
+++ Formula/git-todos.rb
@@ -1,9 +1,9 @@
 class GitTodos < Formula
   desc "A Git based Todos App for Developers"
   homepage "http://git.io/todos"
-  url "https://github.com/ahmed-taj/git-todos/releases/download/v0.2.1/git-todos_0.2.1_darwin_amd64.tar.gz"
-  version "0.2.1"
-  sha256 "dbbaed550269d3685053f8681da11836272dcf5f575036ad4a69123e150b58c9"
+  url "https://github.com/ahmed-taj/git-todos/releases/download/v0.2.2/git-todos_0.2.2_darwin_amd64.tar.gz"
+  version "0.2.2"
+  sha256 "25b5adf77f4a8109f768cb538926f8166595b2236ee275b649d1e5775dacf05a"
   
   depends_on "git"
 
